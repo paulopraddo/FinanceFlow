270f5b5642173e6a04754294a3be6cbcdf3ea142
Paulo Prado <paulopraddo011@gmail.com>
2025-03-13 18:39:51 -0300

    auth service

--- src/main/java/com/prado/paulo/FinanceFlow/app/auth/repository/UserRepository.java
+++ src/main/java/com/prado/paulo/FinanceFlow/app/auth/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.prado.paulo.FinanceFlow.app.auth.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import com.prado.paulo.FinanceFlow.app.auth.provider.model.User;
@@ -8,4 +9,5 @@ import com.prado.paulo.FinanceFlow.app.auth.provider.model.User;
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
 
+    UserDetails findByLogin(String login);
 }
---
+++ src/main/java/com/prado/paulo/FinanceFlow/domain/auth/service/AuthorizationService.java
@@ -0,0 +1,22 @@
+package com.prado.paulo.FinanceFlow.domain.auth.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.prado.paulo.FinanceFlow.app.auth.repository.UserRepository;
+
+@Service
+public class AuthorizationService implements UserDetailsService{
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return this.userRepository.findByLogin(username);
+    }
+
+}
