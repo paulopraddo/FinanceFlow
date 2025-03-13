e3f703839b09e7d7177cb2c5ddb394bf107c5b61
Paulo Prado <paulopraddo011@gmail.com>
2025-03-13 18:05:59 -0300

    initial commit

---
+++ src/test/java/com/prado/paulo/FinanceFlow/FinanceFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prado.paulo.FinanceFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
